116faa10234aa51856963b1101a7dbcb499d4dab
simon-bauer <bauer.simon@gmx.de>
2016-07-16 19:31:59 +0200

    add package

--- init.el
+++ init.el
@@ -1,1 +1,8 @@
-
+;; emacs package system
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list
+   'package-archives
+   '("melpa" . "http://melpa.org/packages/")
+   t)
+  (package-initialize))
