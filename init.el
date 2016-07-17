9da3441fede5a575fc8da22f07eeb36066936125
simon-bauer <bauer.simon@gmx.de>
2016-07-17 23:34:57 +0200

    recent files

--- init.el
+++ init.el
@@ -7,5 +7,15 @@
    t)
   (package-initialize))
 
+;; packages:
+;; - better-defaults
+;; - inf-ruby
+
 ;; org mode
 (setq org-startup-indented t)
+
+;; recent-files
+(require 'recentf)
+(recentf-mode 1)
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
+
