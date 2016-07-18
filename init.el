d082bad66b6fb296a000061a76f22b4b07226614
simon-bauer <bauer.simon@gmx.de>
2016-07-18 22:37:34 +0200

    cua mode

--- init.el
+++ init.el
@@ -17,5 +17,8 @@
 ;; recent-files
 (require 'recentf)
 (recentf-mode 1)
+(setq recentf-max-menu-items 25)
 (global-set-key "\C-x\ \C-r" 'recentf-open-files)
 
+;; Ctrl-x,c,v - cut, copy, paste
+(cua-mode t)
