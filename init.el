98c0b7c32ccacd2c4bbf52229b57a52ff6f1152c
simon-bauer <bauer.simon@gmx.de>
2020-05-16 08:35:22 +0200

    update

--- init.el
+++ init.el
@@ -1,27 +1,201 @@
-;; emacs package system
-(when (>= emacs-major-version 24)
-  (require 'package)
-  (add-to-list
-   'package-archives
-   '("melpa" . "http://melpa.org/packages/")
-   t)
-  (package-initialize))
+;; -*- lexical-binding: t; -*-
 
-;; packages:
-;; - better-defaults
-;; - inf-ruby
-;; - idea-darkula-theme
+;; setup package
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+(package-initialize)
 
-;; org mode
-(setq org-startup-indented t)
+;; Bootstrap use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-when-compile
+  (require 'use-package))
 
-;; recent-files
-(require 'recentf)
-(recentf-mode 1)
-(setq recentf-max-menu-items 25)
-(global-set-key "\C-x\ \C-r" 'recentf-open-files)
+(use-package ivy
+  :ensure t
+  :config  
+  (require 'subr-x)
+  (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t)
+  (setq enable-recursive-minibuffers t))
 
-;; Ctrl-x,c,v - cut, copy, paste
-(cua-mode t)
+(use-package avy
+  :ensure t
+  :bind (( "C-;" . avy-goto-char)))
 
-(load-theme 'idea-darkula t)
+(use-package expand-region
+  :ensure t
+  :bind (("C-=" . er/expand-region)))
+
+(use-package magit
+  :ensure t
+  :config
+  (defadvice server-ensure-safe-dir (around
+                                     my-around-server-ensure-safe-dir
+                                     activate)
+    "Ignores any errors raised from server-ensure-safe-dir"
+    (ignore-errors ad-do-it)))
+
+(use-package ag
+  :ensure t)
+
+(use-package wgrep-ag
+  :ensure t
+  :config
+  ;; enable editing inline in the ag result buffer
+  (autoload 'wgrep-ag-setup "wgrep-ag")
+  (add-hook 'ag-mode-hook 'wgrep-ag-setup))
+
+(use-package clang-format
+  :ensure t)
+
+(use-package origami
+  :ensure t)
+
+(use-package yasnippet
+  :ensure t
+  :config
+  (yas-global-mode 1))
+
+(use-package ggtags
+  :ensure t
+  :config
+  ;; ggtags
+  (add-hook 'c-mode-common-hook
+            (lambda ()
+              (when (derived-mode-p 'c-mode 'c++-mode 'java-mode)
+                (ggtags-mode 1)))))
+
+
+;; which key: on the fly docu for key bindings
+(use-package which-key
+  :ensure t
+  :config
+  (which-key-mode))
+
+;; nicer scrolling
+(use-package smooth-scrolling
+  :ensure t
+  :config
+  (smooth-scrolling-mode 1))
+
+(use-package smartparens
+  :ensure t
+  :init
+  (use-package smartparens-config))
+
+(use-package whole-line-or-region
+  :ensure t
+  :init
+  ;; delete whole line if no region selected
+  (whole-line-or-region-global-mode))
+
+  
+;; mr mode
+;(load "~/.emacs.d/mr.el")
+;(mr-mode)
+;(mr/change-to-last-recent-wasp-without-shell-restart)
+;(mr/startup-shells)
+
+
+
+;; choose a dark theme
+(load-theme 'tango-dark t)
+;; font type and size
+;(set-face-attribute 'default nil :font "Consolas-10" )
+;(set-frame-font "Consolas-10" nil t)
+
+;; suppress welcome buffer, toolbar and scroolbar
+(setq inhibit-splash-screen t)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+(setq c-default-style "bsd"
+      c-basic-offset 4)
+
+;; tabs instead of spaces
+(setq-default indent-tabs-mode nil)
+
+;; open .h files in c++ mode
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+
+(global-display-line-numbers-mode)
+
+;; shorten yes/no answer to y/n
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; Behave like vi's o command
+(defun open-next-line (arg)
+  "Move to the next line and then opens a line.
+    See also `newline-and-indent'."
+  (interactive "p")
+  (end-of-line)
+  (open-line arg)
+  (next-line 1)
+  (when newline-and-indent
+    (indent-according-to-mode)))
+
+(global-set-key (kbd "C-o") 'open-next-line)
+
+;; Behave like vi's O command
+(defun open-previous-line (arg)
+  "Open a new line before the current one. 
+     See also `newline-and-indent'."
+  (interactive "p")
+  (beginning-of-line)
+  (open-line arg)
+  (when newline-and-indent
+    (indent-according-to-mode)))
+
+(global-set-key (kbd "M-o") 'open-previous-line)
+
+
+;; switch between windows
+(windmove-default-keybindings)
+
+
+;; % paren matching
+(global-set-key "%" 'match-paren)
+
+(defun match-paren (arg)
+  "Go to the matching paren if on a paren; otherwise insert %."
+  (interactive "p")
+  (cond ((looking-at "\\s(") (forward-list 1) (backward-char 1))
+	((looking-at "\\s)") (forward-char 1) (backward-list 1))
+	(t (self-insert-command (or arg 1)))))
+
+
+
+;; revert without confirmation
+(defun revert-buffer-without-confirmation ()
+  (interactive)
+  (revert-buffer t t))
+(global-set-key (kbd "<f5>") 'revert-buffer-without-confirmation)
+
+
+
+;; keep history of window configurations
+(winner-mode 1)
+
+;; e.g. a yank replaces current region
+(delete-selection-mode 1)
+
+;; easier eval-buffer
+(global-set-key (kbd "C-c e") 'eval-buffer)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (smartparens yasnippet whole-line-or-region which-key wgrep-ag use-package swiper smooth-scrolling pkg-info pfuture origami magit hydra ht ggtags f expand-region clang-format auto-complete ag ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ reaction-shell.el
@@ -0,0 +1,96 @@
+;; -*- lexical-binding: t; -*-
+
+;;;;;;;;;;;;;;;;;;;;;
+;; external interface
+
+(defun make-reaction-shell (shell-name-string init-cmd-string-list)
+  (interactive)
+  (kill-reaction-shell-buffer-if-exist shell-name-string)
+  (let ((shell-hash (make-hash-table :test 'equal)))
+    (puthash :init-cmd-string-list init-cmd-string-list shell-hash)
+    (puthash :initialized nil shell-hash)
+    (puthash :mutex (make-mutex) shell-hash)
+    (puthash :queue () shell-hash)
+    (puthash :shell-busy nil shell-hash)
+    (puthash :current-output-hook-action (lambda ()) shell-hash)
+    (puthash shell-name-string shell-hash reaction-shell-registry)))
+
+(defun queue-reaction-shell-cmd (shell-name-string shell-cmd-string reaction-function)
+  "The pair of shell-cmd and reaction-function is queued for the given shell.\
+The shell-cmd is started as soon as the existing cmd pairs in the queue are\
+finished. The reaction-function is invoced as soon as the corresponding\
+shell-cmd is finished.\
+If the given shell is not running it is started and initialized."
+  (interactive)
+  (let ((buffer-name (reaction-shell-buffer-name shell-name-string)))
+        (shell buffer-name)
+        (unless (gethash :initialized (gethash shell-name-string reaction-shell-registry))
+          (reaction-shell-queue-cmd-pairs-internal shell-name-string
+           (mapcar (lambda (cmd-string) (cons cmd-string (lambda ()) ))
+                   (gethash :init-cmd-string-list (gethash shell-name-string reaction-shell-registry))))
+          (puthash :initialized t (gethash shell-name-string reaction-shell-registry)))
+        (reaction-shell-queue-cmd-pairs-internal
+         shell-name-string
+         (list (cons shell-cmd-string reaction-function)))))
+
+;; internal mechanism:
+;; - each shell-command is appended the insertion of an end marker
+;; - before each shell-command the action-hook of the buffer is set to the reaction-function
+;; - before an reaction function the actino-hook is set to an empty function
+;; - after an reaction function a cmd-pair-finished trigger is activated
+;; - changes on state :queue/:shell-busy must be protected by mutex
+ 
+;; - states:
+;;          |
+;;          | cmd-pair is finished
+;;          v
+;;   - queue empty, shell not busy
+;;          |
+;;          | cmd-pair is queued
+;;          v
+;;   - queue has entry, shell not busy
+;;          |
+;;          | cmd-pair is taken from queue and given to shell
+;;          v
+;;   - queue empty, shell busy
+;;          |
+;;          | cmd-pair is queued
+;;          v 
+;;   - queue has entry, shell busy
+;;          |
+;;          | cmd-pair is finished
+;;          v
+;;   - queue has entry, shell not busy
+;;          |
+;;          | cmd-pair is taken from queue and given to shell
+;;          v
+;;   - queue empty, shell busy
+;;          |
+;; - main trigger:
+;;   - cmd-pair-queued
+;;   - cmd-pair-finished
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; internal functions and data
+
+
+(defun kill-reaction-shell-buffer-if-exist (shell-name-string)
+  ;; remove confirmation for process buffers
+  (setq kill-buffer-query-functions
+	(delq 'process-kill-buffer-query-function kill-buffer-query-functions))
+  (let ((buffer-name (reaction-shell-buffer-name shell-name-string)))
+        (when (get-buffer buffer-name)
+            (kill-buffer buffer-name))))
+
+(defun reaction-shell-buffer-name (shell-name-string)
+  (concat "*" shell-name-string "*"))
+
+(defun reaction-shell-queue-cmd-pairs-internal (shell-name-string list-cmd-pairs)
+  (with-mutex (gethash :mutex (gethash shell-name-string reaction-shell-registry))
+    ))
+
+
+;; hash-table containing for each shell another hash-table with all shell related variables
+(setq reaction-shell-registry (make-hash-table :test 'equal))
