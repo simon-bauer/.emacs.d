ad79cfb7bc725500868de066188ae79e344fb67d
simon-bauer <bauer.simon@gmx.de>
2020-05-19 21:40:07 +0200

    rename to shorter names

--- reaction-shell-tests.el
+++ reaction-shell-tests.el
@@ -1,32 +1,44 @@
 (describe "reaction-shell"
   (after-each
-    (setq reaction-shell-registry (make-hash-table :test 'equal)))
+    (setq rs-registry (make-hash-table :test 'equal)))
   
-  (describe "make-reaction-shell"
+  (describe "rs-init"
     (it "adds a new entry in the global registry"
-      (make-reaction-shell "test-shell" ())
-      (expect (gethash "test-shell" reaction-shell-registry) :not :to-equal nil)))
+      (rs-init "test-shell" ())
+      (expect (gethash "test-shell" rs-registry) :not :to-equal nil)))
 
-  (describe "reaction-shell-queue-cmd"
+  (describe "rs-cmd"
     (before-each
-      (make-reaction-shell "test-shell" '("init cmd")))
+      (rs-init "test-shell" '("init cmd")))
     (it "starts a new shell buffer if it is not already running"
-      (reaction-shell-queue-cmd "test-shell" "ls" 'ignore)
-      (let ((first-buffer (get-buffer (reaction-shell-buffer-name "test-shell"))))
+      (rs-cmd "test-shell" "ls" 'ignore)
+      (let ((first-buffer (get-buffer (rs-buffer-name "test-shell"))))
         (expect first-buffer :not :to-equal nil)
-        (reaction-shell-queue-cmd "test-shell" "ls" 'ignore)
-        (expect (get-buffer (reaction-shell-buffer-name "test-shell")) :to-be first-buffer)))
+        (rs-cmd "test-shell" "ls" 'ignore)
+        (expect (get-buffer (rs-buffer-name "test-shell")) :to-be first-buffer)))
     (it "queues the initial commands when run for the first time"
-      (spy-on 'reaction-shell-queue-cmd-pairs-internal)
-      (reaction-shell-queue-cmd "test-shell" "ls" 'ignore)
-      (expect 'reaction-shell-queue-cmd-pairs-internal :to-have-been-called-with "test-shell" '(("init cmd" . ignore))))
+      (spy-on 'rs-cmd-internal)
+      (rs-cmd "test-shell" "ls" 'ignore)
+      (expect 'rs-cmd-internal :to-have-been-called-with "test-shell" '(("init cmd" . ignore))))
     (it "queues the shell cmd together with the reaction"
-      (spy-on 'reaction-shell-queue-cmd-pairs-internal)
-      (reaction-shell-queue-cmd "test-shell" "ls" 'ignore)
-      (expect 'reaction-shell-queue-cmd-pairs-internal :to-have-been-called-with "test-shell" '(("ls" . ignore)))))
+      (spy-on 'rs-cmd-internal)
+      (rs-cmd "test-shell" "ls" 'ignore)
+      (expect 'rs-cmd-internal :to-have-been-called-with "test-shell" '(("ls" . ignore)))))
 
-  (describe "reaction-shell-queue-cmd-pairs-internal"
+  (describe "rs-cmd-internal"
+    (before-each
+      (spy-on 'rs-trigger)
+      (rs-init "test-shell" ())
+      (rs-cmd-internal "test-shell" '(("ls" . ignore))))
     (it "appends the given cmd pairs to the queue in the registry"
-      (make-reaction-shell "test-shell" ())
-      (reaction-shell-queue-cmd-pairs-internal "test-shell" '(("ls" . ignore)))
-      (expect (gethash :queue (gethash "test-shell" reaction-shell-registry)) :to-equal '(("ls" . ignore)))) ))
+      (expect (gethash :queue (gethash "test-shell" rs-registry)) :to-equal '(("ls" . ignore))))
+    (it "triggers the check if the next cmd pair can be executed"
+      (expect 'rs-trigger :to-have-been-called)))
+
+  (describe "rs-trigger"
+    (before-each
+      (spy-on 'rs-process-next)
+      (rs-init "test-shell" ()))
+    (it "calls process next cmd pair when queue not empty and shell not busy"
+      (rs-trigger "test-shell")
+      (expect 'rs-process-next :not :to-have-been-called))))
--- reaction-shell.el
+++ reaction-shell.el
@@ -5,39 +5,39 @@
 
 (provide 'reaction-shell)
 
-(defun make-reaction-shell (shell-name-string init-cmd-string-list)
+(defun rs-init (shell-name init-cmd-list)
   (interactive)
-  (kill-reaction-shell-buffer-if-exist shell-name-string)
+  (rs-kill-buffer shell-name)
   (let ((shell-hash (make-hash-table :test 'equal)))
-    (puthash :init-cmd-string-list init-cmd-string-list shell-hash)
+    (puthash :init-cmd-list init-cmd-list shell-hash)
     (puthash :initialized nil shell-hash)
     (puthash :mutex (make-mutex) shell-hash)
     (puthash :queue () shell-hash)
     (puthash :shell-busy nil shell-hash)
-    (puthash :current-output-hook-action (lambda ()) shell-hash)
-    (puthash shell-name-string shell-hash reaction-shell-registry)))
+    (puthash :current-output-hook-action 'ignore shell-hash)
+    (puthash shell-name shell-hash rs-registry)))
 
-(defun reaction-shell-queue-cmd (shell-name-string shell-cmd-string reaction-function)
-  "The pair of shell-cmd and reaction-function is queued for the given shell.\
-The shell-cmd is started as soon as the existing cmd pairs in the queue are\
-finished. The reaction-function is invoced as soon as the corresponding\
-shell-cmd is finished.\
+(defun rs-cmd (shell-name shell-cmd callback)
+  "The pair of shell-cmd and callback is queued for the given shell.
+The shell-cmd is started as soon as the existing cmd pairs in the queue are
+finished. The callback is invoced as soon as the corresponding
+shell-cmd is finished.
 If the given shell is not running it is started and initialized."
   (interactive)
-  (let ((buffer-name (reaction-shell-buffer-name shell-name-string)))
+  (let ((buffer-name (rs-buffer-name shell-name)))
         (shell buffer-name)
-        (unless (gethash :initialized (gethash shell-name-string reaction-shell-registry))
-          (reaction-shell-queue-cmd-pairs-internal shell-name-string
+        (unless (gethash :initialized (gethash shell-name rs-registry))
+          (rs-cmd-internal shell-name
            (mapcar (lambda (cmd-string) (cons cmd-string 'ignore ))
-                   (gethash :init-cmd-string-list (gethash shell-name-string reaction-shell-registry))))
-          (puthash :initialized t (gethash shell-name-string reaction-shell-registry)))
-        (reaction-shell-queue-cmd-pairs-internal
-         shell-name-string
-         (list (cons shell-cmd-string reaction-function)))))
+                   (gethash :init-cmd-list (gethash shell-name rs-registry))))
+          (puthash :initialized t (gethash shell-name rs-registry)))
+        (rs-cmd-internal
+         shell-name
+         (list (cons shell-cmd callback)))))
 
 ;; internal mechanism:
 ;; - each shell-command is appended the insertion of an end marker
-;; - before each shell-command the action-hook of the buffer is set to the reaction-function
+;; - before each shell-command the action-hook of the buffer is set to the callback
 ;; - before an reaction function the actino-hook is set to an empty function
 ;; - after an reaction function a cmd-pair-finished trigger is activated
 ;; - changes on state :queue/:shell-busy must be protected by mutex
@@ -78,23 +78,32 @@ If the given shell is not running it is started and initialized."
 ;; internal functions and data
 
 
-(defun kill-reaction-shell-buffer-if-exist (shell-name-string)
+(defun rs-kill-buffer (shell-name)
   ;; remove confirmation for process buffers
   (setq kill-buffer-query-functions
 	(delq 'process-kill-buffer-query-function kill-buffer-query-functions))
-  (let ((buffer-name (reaction-shell-buffer-name shell-name-string)))
+  (let ((buffer-name (rs-buffer-name shell-name)))
         (when (get-buffer buffer-name)
             (kill-buffer buffer-name))))
 
-(defun reaction-shell-buffer-name (shell-name-string)
-  (concat "*" shell-name-string "*"))
+(defun rs-buffer-name (shell-name)
+  (concat "*" shell-name "*"))
 
-(defun reaction-shell-queue-cmd-pairs-internal (shell-name-string cmd-pair-list)
-  (with-mutex (gethash :mutex (gethash shell-name-string reaction-shell-registry))
+(defun rs-cmd-internal (shell-name cmd-pair-list)
+  (with-mutex (gethash :mutex (gethash shell-name rs-registry))
     (puthash :queue 
-             (append (gethash :queue (gethash shell-name-string reaction-shell-registry)) cmd-pair-list)
-             (gethash shell-name-string reaction-shell-registry))))
+             (append (gethash :queue (gethash shell-name rs-registry)) cmd-pair-list)
+             (gethash shell-name rs-registry)))
+  (rs-trigger shell-name))
 
+(defun rs-trigger (shell-name)
+  (let ((queue-empty (not (gethash :queue (gethash shell-name rs-registry))))
+        (shell-busy (gethash :shell-busy (gethash shell-name rs-registry))))
+    (when (and (not queue-empty) (not shell-busy))
+      (rs-process-next shell-name))))
+
+(defun rs-process-next (shell-name)
+  )
 
 ;; hash-table containing for each shell another hash-table with all shell related variables
-(setq reaction-shell-registry (make-hash-table :test 'equal))
+(setq rs-registry (make-hash-table :test 'equal))
