fa129e4438e5b1fb6fed846baaa1aee10a5cadb1
simon-bauer <bauer.simon@gmx.de>
2020-06-29 21:01:18 +0200

    work on kauri-config

--- init.el
+++ init.el
@@ -202,22 +202,41 @@
 
 ;; format: (type properties child1 child2 ...)
 
-(defun kauri-plist (org-ast)
-  "convert headlines of an org ast to a nested plist"
-  (cond ((eq (car org-ast) 'org-data)
-         (kauri-plist-2 (cddr org-ast)))
-        ((eq (car org-ast) 'headline)
-         (cons
-          (plist-get (cadr org-ast) :title)
-          (kauri-plist-2 (cddr org-ast))))))
+(defun kauri-config-is-root (org-element)
+  (eq (car org-element) 'org-data))
 
-(defun kauri-plist-2 (org-element-list)
-  (if org-element-list
-      (cons (kauri-plist (car org-element-list))
-            (kauri-plist-2 (cdr org-element-list)))
+(defun kauri-config-is-key (org-element)
+  (and (eq (car org-element) 'headline)
+       (> (length org-element) 2)))
+
+(defun kauri-config-is-value (org-element)
+  (and (eq (car org-element) 'headline)
+       (equal (length org-element) 2)))
+
+(defun kauri-config-get-key (org-element)
+  (plist-get (cadr org-element) :title))
+
+(defun kauri-config-get-value (org-element)
+  (kauri-config-get-key org-element))
+
+(defun kauri-config-plist (org-ast)
+  "convert headlines of an org ast to a nested plist"
+  (cond ((kauri-config-is-root org-ast)
+         (kauri-config-plist-2 (cddr org-ast)))
+        ((kauri-config-is-key org-ast)
+         (cons
+          (kauri-config-get-key org-ast)
+          (kauri-config-plist-2 (cddr org-ast))))
+        ((kauri-config-is-value org-ast)
+         (kauri-config-get-value org-ast))))
+
+(defun kauri-config-plist-2 (org-child-list)
+  (if org-child-list
+      (cons (kauri-config-plist (car org-child-list))
+            (kauri-config-plist-2 (cdr org-child-list)))
     nil))
 
-(kauri-plist (with-current-buffer "kauri.org"
+(kauri-config-plist (with-current-buffer "kauri.org"
                (org-element-parse-buffer 'headline)))
 
 
